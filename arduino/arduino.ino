25434bb931009d7f7095ac5a94576c69e71ca698
이소용 <dlthfyd708@naver.com>
2021-08-28 21:17:42 +0900

    추가 - 이전 커밋 기능 보완

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -2,18 +2,34 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
 
+
+
 // Set these to run example.
-#define FIREBASE_HOST ""
-#define FIREBASE_AUTH ""
-#define WIFI_SSID ""
-#define WIFI_PASSWORD ""
+#define FIREBASE_HOST "arduino-541d0-default-rtdb.asia-southeast1.firebasedatabase.app"
+#define FIREBASE_AUTH "R3RGvY2LloKhe5XfIjWksTr7w7ZovVmCIMZu643P"
+#define WIFI_SSID "U+Net1D1B"
+#define WIFI_PASSWORD "0192023420"
+
+//D6~11 플래시메모리용으로 사용중
+//D0,2,15 부팅모드 설정용 사용시 주의
+//D1,3 시리얼 통신용
+//D16 Wake용
 
 #define led_IN 4
-#define button_OUT 5
-#define pir_OUT 6
-#define buzzer_IN 7
+#define button_OUT 7
+#define buzzer_IN 5
 #define voice_TX 8
 #define voice_RX 9
+
+const int GPIO_1 = 0;
+const int led = 0;
+
+uint8_t pir_OUT = 6;
+
+#define wifi_RX 2
+#define wifi_TX 3
+
+SoftwareSerial mywifi(wifi_RX, wifi_TX);
 
 //#define voice_emergency 111 // 음성 응급호출 111 전송
 //#define button_emergency 112 // 버튼 응급호출 112 전송
@@ -33,21 +49,25 @@ int button_push_time = 0; // 버튼 눌러지는 시간
 int button_emergency = 0; // 버튼호출 표준 0, 호출 1
 int voice_emergency = 0; // 음성호출 표준 0, 호출 1
 int cancel_signal = 0; // 표준 0, 응급호출 신호 취소 1 
+int pir_delayed_time = 0; 
 int delayed_time = 0; // 와이파이 접속 끊어진 시간
 
 
 void setup() {
-  Serial.begin(9600);
-
+  
   // 센서 디지털 핀 모드 설정
-  pinMode(button_OUT, INPUT);
   pinMode(pir_OUT, INPUT);
+  pinMode(button_OUT, INPUT);
   pinMode(led_IN, OUTPUT);
   pinMode(buzzer_IN, OUTPUT);
+  
+  Serial.begin(9600);
+  mywifi.begin(9600);
+
 
   //와이파이 연결
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("와이파이 연결 중");
+  Serial.print("connecting");
   
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
@@ -55,7 +75,7 @@ void setup() {
   }
   
   Serial.println();
-  Serial.println("와이파이 연결 성공!");
+  Serial.println("connected!");
   Serial.print("SSID : ");
   Serial.println(WiFi.localIP());
 
@@ -80,6 +100,7 @@ void loop() {
     inout_check();
     button_check();
     send_signal();
+    Serial.println("loop()");
   
   }else{
     
@@ -103,20 +124,21 @@ void loop() {
     Serial.print("지연시간 : ");
     Serial.print(delayed_time);
     Serial.println("(초)");
+    pir_delayed_time += delayed_time;
   }
-  
-  delay(200);
+
 }
 
 
 void inout_check(){
 
   int value = digitalRead(pir_OUT);
-
+  Serial.print("value : ");
+  Serial.println(value);
   if(value == HIGH && count == 0){ // 들어오는 경우
     
     Serial.println("들어옴");
-    count = 1;
+    pir_delayed_time = 0;
     entered = 1;
     //처음 입장시간 저장
     //부저 함수 추가
@@ -124,16 +146,21 @@ void inout_check(){
   }else if(value == HIGH && count != 0){ // 안에서 움직이는 경우
     
     Serial.println("내부활동");
+    count = 1;
+    pir_delayed_time = 0;
     
-  }else if(time_delayed >= 30){ // 나가는 경우 (내부활동 30초 이상 없을경우)
+  }else if(pir_delayed_time >= 30){ // 나가는 경우 (내부활동 30초 이상 없을경우)
     
     Serial.println("나감");
+    pir_delayed_time = 0;
     count = 0;
     entered = 2;
     //입장시간 초기화
-    
+
   }  
-  
+
+  pir_delayed_time++;
+  delay(1000);  
 }
 
 
@@ -142,14 +169,15 @@ void button_check(){
   int value = digitalRead(button_OUT);
 
   if(value == HIGH){
-    while(value != LOW || button_pushed_time != 3){
-      button_pushed_time++;
+    while(value != LOW || button_push_time != 3){
+      button_push_time++;
+      pir_delayed_time++;
       value = digitalRead(button_OUT);
       delay(1000);
     }
   }
 
-  if(button_pushed_time <= 2){ // 일반적 응급호출 (최대 2.9초 까지 응급호출로 인식)
+  if(button_push_time <= 2){ // 일반적 응급호출 (최대 2.9초 까지 응급호출로 인식)
     
     button_emergency = 1;
     
@@ -177,20 +205,22 @@ void button_check(){
     
   }
 
-  button_pushed_time = 0;
+  button_push_time = 0;
   
 }
 
 void send_signal(){  // 중요 - 모든 응급신호 10초간 대기하고 10초 안에 취소 버튼 누를경우 응급신호 보내지 않음
 
   if(entered == 1 && count == 0){
-  Firebase.setInt("출입상태", 1);
+    Firebase.setString("enter", "true");
+    Serial.println("파이어베이스 입장 데이터 전송");
+    count++;
     // handle error
     if (Firebase.failed()) {
       
         Serial.print("출입상태 전송 오류 : ");
         Serial.println(Firebase.error());  
-        
+        count = 0;
         
     }
   }
@@ -198,7 +228,8 @@ void send_signal(){  // 중요 - 모든 응급신호 10초간 대기하고 10초
 
   if(entered == 2){
     
-  Firebase.setInt("출입상태", 0);
+    Firebase.setString("enter", "false");
+        Serial.println("파이어베이스 퇴장 데이터 전송");
     // handle error
     if (Firebase.failed()) {
       
