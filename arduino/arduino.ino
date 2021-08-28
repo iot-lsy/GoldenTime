f5a9b90c9bc0ead22819c9f81a9a7c282ee49dad
이소용 <dlthfyd708@naver.com>
2021-08-28 16:03:03 +0900

    추가 - 출입센서 파이어베이스 데이터 전송 기능 구현

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -15,23 +15,24 @@
 #define voice_TX 8
 #define voice_RX 9
 
-#define voice_emergency 111 // 음성 응급호출 111 전송
-#define button_emergency 112 // 버튼 응급호출 112 전송
-#define button_emergency_cancel 113 // 응급호출 취소 113 전송
-#define person_in 121 // pir센서 들어오는 경우 121 전송
-#define person_out 122 // pir센서 나가는 경우 122 전송
+//#define voice_emergency 111 // 음성 응급호출 111 전송
+//#define button_emergency 112 // 버튼 응급호출 112 전송
+//#define button_emergency_cancel 113 // 응급호출 취소 113 전송
+//#define person_in 121 // pir센서 들어오는 경우 121 전송
+//#define person_out 122 // pir센서 나가는 경우 122 전송
 
 void inout_check(); // 출입감지 함수
-void filtering_signal(); // 센싱 데이터 필터링 함수
 void send_signal(); // 파이어베이스 데이터 전송 함수
 void button_check(); // 버튼 센싱 함수
+void voice_check(); // 음성 센싱 함수
+void buzzer_out(int); // 부저 울리는 함수 (매개변수 0 : 취소알림소리, 1 : 응급상황알림소리, 2 : 입장알림소리)
 
-int entered = 0; // 들어감 1, 나감 0
+int entered = 0; // 표준 0, 들어감 1, 나감 2
 int count = 0; // 활동감지 횟수
 int button_push_time = 0; // 버튼 눌러지는 시간
-int button_emergency = 0; // 버튼호출 표준 0 호출 1
-int voice_emergency = 0; // 음성호출 표준 0 호출 1
-int cancel_signal = 0; // 표준 0 응급호출 신호 취소 1 
+int button_emergency = 0; // 버튼호출 표준 0, 호출 1
+int voice_emergency = 0; // 음성호출 표준 0, 호출 1
+int cancel_signal = 0; // 표준 0, 응급호출 신호 취소 1 
 int delayed_time = 0; // 와이파이 접속 끊어진 시간
 
 
@@ -76,8 +77,9 @@ void loop() {
 
   if(WiFi.status() == WL_CONNECTED && !Firebase.failed()){
     
-  inout_check();
-  button_check();
+    inout_check();
+    button_check();
+    send_signal();
   
   }else{
     
@@ -103,13 +105,13 @@ void loop() {
     Serial.println("(초)");
   }
   
-  
+  delay(200);
 }
+
 
 void inout_check(){
 
   int value = digitalRead(pir_OUT);
-
 
   if(value == HIGH && count == 0){ // 들어오는 경우
     
@@ -117,6 +119,7 @@ void inout_check(){
     count = 1;
     entered = 1;
     //처음 입장시간 저장
+    //부저 함수 추가
     
   }else if(value == HIGH && count != 0){ // 안에서 움직이는 경우
     
@@ -126,11 +129,10 @@ void inout_check(){
     
     Serial.println("나감");
     count = 0;
-    entered = 0;
+    entered = 2;
     //입장시간 초기화
     
-  }
-  
+  }  
   
 }
 
@@ -140,14 +142,14 @@ void button_check(){
   int value = digitalRead(button_OUT);
 
   if(value == HIGH){
-    while(value != LOW){
+    while(value != LOW || button_pushed_time != 3){
       button_pushed_time++;
       value = digitalRead(button_OUT);
       delay(1000);
     }
   }
 
-  if(button_pushed_time<=2){ // 일반적 응급호출 (최대 2.9초 까지 응급호출로 인식)
+  if(button_pushed_time <= 2){ // 일반적 응급호출 (최대 2.9초 까지 응급호출로 인식)
     
     button_emergency = 1;
     
@@ -158,12 +160,14 @@ void button_check(){
       Serial.println("버튼 응급호출 취소");
       button_emergency = 0;
       cancel_signal = 1;
+      //부저 함수 추가
       
     }else if(voice_emergency){
       
       Serial.println("음성 응급호출 취소");
       voice_emergency = 0;
       cancel_signal = 1;
+      //부저 함수 추가
       
     }else{
       
@@ -173,34 +177,44 @@ void button_check(){
     
   }
 
-  
-  
-}
-
-void filtering_siganl(){
+  button_pushed_time = 0;
   
 }
 
-void send_signal(){
+void send_signal(){  // 중요 - 모든 응급신호 10초간 대기하고 10초 안에 취소 버튼 누를경우 응급신호 보내지 않음
+
+  if(entered == 1 && count == 0){
+  Firebase.setInt("출입상태", 1);
+    // handle error
+    if (Firebase.failed()) {
+      
+        Serial.print("출입상태 전송 오류 : ");
+        Serial.println(Firebase.error());  
+        
+        
+    }
+  }
+  
+
+  if(entered == 2){
+    
+  Firebase.setInt("출입상태", 0);
+    // handle error
+    if (Firebase.failed()) {
+      
+        Serial.print("setting /number failed:");
+        Serial.println(Firebase.error());
+        
+    }else{ // 퇴장과 동시에 출입센서 초기화
+      entered = 0;
+      count = 0;
+    }
+  }
+
+  //버튼 전송 조건문 
+
+
   /*
-  Firebase.setFloat("number", 42.0);
-  // handle error
-  if (Firebase.failed()) {
-      Serial.print("setting /number failed:");
-      Serial.println(Firebase.error());  
-      return;
-  }
-  delay(1000);
-  
-  // update value
-  Firebase.setFloat("number", 43.0);
-  // handle error
-  if (Firebase.failed()) {
-      Serial.print("setting /number failed:");
-      Serial.println(Firebase.error());  
-      return;
-  }
-  delay(1000);
   // get value 
   Serial.print("number: ");
   Serial.println(Firebase.getFloat("number"));
